a65ee08c9ab38e497bbfcd1efb03cbd00ac3cf25
jandagacor303 <jandagacor303@gmail.com>
2025-01-13 00:34:24 +0700

    Create backlink.php

---
+++ backlink.php
@@ -0,0 +1,20 @@
+<div style="display:none">
+<a href="https://majupantangmundur.pro/">https://majupantangmundur.pro/</a>
+<a href="https://heylink.me/jandagacormantap">https://heylink.me/jandagacormantap</a>
+<a href="https://journal.unuha.ac.id/ayokgas/">https://journal.unuha.ac.id/ayokgas/</a>
+<a href="https://jikesi.fk.unand.ac.id/">https://jikesi.fk.unand.ac.id/</a>
+<a href="https://partnership.uniba-bpn.ac.id/ayokmain/">https://partnership.uniba-bpn.ac.id/ayokmain/</a>
+<a href="https://firmas.velasresorts.com/">https://firmas.velasresorts.com/</a>
+<a href="https://sardenberg.com.br/">https://sardenberg.com.br/</a>
+<a href="https://www.yamauchi.com.br/data/">https://www.yamauchi.com.br/data/</a>
+<a href="https://www.yamauchi.com.br/">https://www.yamauchi.com.br/</a>
+<a href="https://presensi.unwim.ac.id/ayokgas/">https://presensi.unwim.ac.id/ayokgas/</a>
+<a href="https://kti.cbt.stisi.ac.id/">https://kti.cbt.stisi.ac.id/</a>
+<a href="https://joanneencarnacion.velasresorts.com/">https://joanneencarnacion.velasresorts.com/</a>
+<a href="https://ndt.pnec.nust.edu.pk/">https://ndt.pnec.nust.edu.pk/</a>
+<a href="https://bsdmadum.umtas.ac.id/ayokgas/">https://bsdmadum.umtas.ac.id/ayokgas/</a>
+<a href="https://olympiad.pnec.nust.edu.pk/">https://olympiad.pnec.nust.edu.pk/</a>
+<a href="https://sso.cbt.stisi.ac.id/">https://sso.cbt.stisi.ac.id/</a>
+<a href="https://sto.cbt.stisi.ac.id/">https://sto.cbt.stisi.ac.id/</a>
+<a href="https://ti.cbt.stisi.ac.id/">https://ti.cbt.stisi.ac.id/</a>
+</div>
