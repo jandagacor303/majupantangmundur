925e0d062f37ba63480a727c62646949723516eb
jandagacor303 <jandagacor303@gmail.com>
2025-01-20 01:10:47 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -3,6 +3,5 @@
 <a href="https://directorio-gvrn.velasresorts.com/">https://directorio-gvrn.velasresorts.com/</a>
 <a href="https://presensi.unwim.ac.id/">https://presensi.unwim.ac.id/</a>
 <a href="https://heylink.me/jandagacormantap">https://heylink.me/jandagacormantap</a>
-<a href="https://firmas.velasresorts.com/">https://firmas.velasresorts.com/</a>
 <a href="https://presensi.unwim.ac.id/ayokgas/">https://presensi.unwim.ac.id/ayokgas/</a>
 </div>
